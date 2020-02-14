819289180775f152edde0c6d6066b0ddda0be779
cathyamart <60454821+cathyamart@users.noreply.github.com>
2020-02-14 03:09:57 +0000

    Add files via upload

---
+++ linkedin_api_signup/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("CLIENT_ID", "77i1z7bt9rg18n");
+define("CLIENT_SECRET", "M7lvdKgVUxZjoWAP");
+define("REDIRECT_URI", "http://localhost/linkedin_api_signup/index.php");
+define("SCOPE", 'r_basicprofile r_emailaddress' );
+
+?>
